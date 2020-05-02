13487a015ad47c31d3d9921f393cd010918382e5
Rebuking <rebuking.blade@gmail.com>
2020-05-02 18:17:10 +0300

    Added Mongoose Schema for Users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/users
+// @route   Post api/users
 // @desc    Test route
 // @access  Public
 router.get('/', (req, res) => res.send("User Route"));
