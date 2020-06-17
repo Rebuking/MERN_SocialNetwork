90f6738c24f7b96f99a83b2cede801e27927e1ef
Rebuking <rebuking.blade@gmail.com>
2020-06-17 13:39:26 +0300

    Login and Register Forms

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,52 @@
+import React, { Fragment, useState } from "react";
+// import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: ""
+	});
+
+	const { email, password } = formData;
+	const onChange = (e) =>
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+	};
+	return (
+		<Fragment>
+			<h1 className='large text-primary'>Sign In</h1>
+			<p className='lead'>
+				<i className='fas fa-user'></i> Sing Into Your Account
+			</p>
+			<form className='form' onSubmit={(e) => onSubmit(e)}>
+				<div className='form-group'>
+					<input
+						type='email'
+						placeholder='Email Address'
+						name='email'
+						value={email}
+						onChange={(e) => onChange(e)}
+						required
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Password'
+						name='password'
+						value={password}
+						onChange={(e) => onChange(e)}
+						minLength='6'
+					/>
+				</div>
+				<input type='submit' class='btn btn-primary' value='Login' />
+			</form>
+			<p className='my-1'>
+				You don't have an account? <Link to='/register'>Sign Up</Link>
+			</p>
+		</Fragment>
+	);
+}
